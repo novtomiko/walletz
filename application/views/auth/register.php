f9132133a7e4f7f6042428c46833e9f810692667
Yudistira Dwi Septian Rubiyanto <yudistiradsr@gmail.com>
2023-07-04 19:41:11 +0700

    defect fix 1

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -13,7 +13,7 @@
                 <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Nama Lengkap" value="<?= set_value('name'); ?>"> <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Alamat Email" value="<?= set_value('email'); ?>"> <?= form_error('email', '<smallclass="text-danger pl-3">', '</small>'); ?>
+                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Alamat E-mail" value="<?= set_value('email'); ?>"> <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
               </div>
               <div class="form-group row">
                 <div class="col-sm-6 mb-3 mb-sm-0">
